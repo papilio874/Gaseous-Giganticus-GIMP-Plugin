370af0ec6955aaa6054ffdc4f2ba2a91edab085c
papilio874 <69399025+papilio874@users.noreply.github.com>
2020-08-09 17:11:47 -0600

    Update gaseous-giganticus-gimp-plugin.c

--- gaseous-giganticus-gimp-plugin.c
+++ gaseous-giganticus-gimp-plugin.c
@@ -35,6 +35,8 @@
 #include "png_utils.h"
 #include "pthread_util.h"
 
+// - - - - - ◊ PUT ALL DEFINITIONS HERE ◊ - - - - - //
+
 // - - - - - ◊ - - - - - ◊ GIMP PLUGIN PREREQUISITES ◊ - - - - - ◊ - - - - - //
 
 // - - - - - ◊ Delare local functions ◊ - - - - - //
@@ -51,5 +53,40 @@ const GimpPlugInInfo PLUG_IN_INFO =
 };
 
 // - - - - - ◊ - - - - - ◊ GIMP PLUGIN PROGRAM ◊ - - - - - ◊ - - - - - //
+
+// - - - - - ◊
+static gint32
+gaseous_giganticus (GimpDrawable *drawable,
+                    gint32       *layer_id) //MUST be ordered: generate >> create new images for cubemap faces > apply generated map to cubemap in order to assure seamlessness
+}
+  gint32 new_image_id_pos_z
+  GimpDrawable *new_layer
+  //????
+  
+  // - - - - - ◊ - - - - - ◊ CREATE CUBEMAP FACE: NEGATIVE Z (SOUTH POLE) ◊ - - - - - ◊ - - - - - // //maps to image "0" in forum post and original image output
+  new_image_id_neg_z = gimp_image_new (config.cubemap_dimension, config.cubemap_dimension, GIMP_RGB);
+  //...
+  return new_image_id_neg_z;
+  
+  // - - - - - ◊ - - - - - ◊ CREATE CUBEMAP FACE: POSITIVE X (EQUATOR QUADRANT 1) ◊ - - - - - ◊ - - - - - // //maps to image "1" in forum post and original program output.
+  new_image_id_pos_x = gimp_image_new (config.cubemap_dimension, config.cubemap_dimension, GIMP_RGB);
+  //...
+  return new_image_id_pos_x;
+  
+  // - - - - - ◊ - - - - - ◊ CREATE CUBEMAP FACE: POSITIVE Z (NORTH POLE) ◊ - - - - - ◊ - - - - - //  //maps to image "2" in forum post and original program output
+  new_image_id_pos_z = gimp_image_new (config.cubemap_dimension, config.cubemap_dimension, GIMP_RGB);
+  //...
+  return new_image_id_pos_z;
+  
+  // - - - - - ◊ - - - - - ◊ CREATE CUBEMAP FACE: NEGATIVE X (EQUATOR QUADRANT 3) ◊ - - - - - ◊ - - - - - // //maps to image "3" in forum post
+  new_image_id_neg_x = gimp_image_new (config.cubemap_dimension, config.cubemap_dimension, GIMP_RGB);
+  //...
+  return new_image_id_neg_x;
+  
+  // - - - - - ◊ - - - - - ◊ CREATE CUBEMAP FACE: POSITIVE Y (EQUATOR QUADRANT 2) ◊ - - - - - ◊ - - - - - // //maps to image "4"
+  
+  // - - - - - ◊ - - - - - ◊ CREATE CUBEMAP FACE: NEGATIVE Y (EQUATOR QUADRANT 4) ◊ - - - - - ◊ - - - - - // //maps to image "5"
+  
+}
 
 // - - - - - ◊ - - - - - ◊ GIMP PLUGIN USER INTERFACE ◊ - - - - - ◊ - - - - - //

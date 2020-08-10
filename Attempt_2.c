c78d1046f1cc443e11e0c28bd8949e08b63daf2b
papilio874 <69399025+papilio874@users.noreply.github.com>
2020-08-10 10:11:19 -0600

    Update Attempt_2.c

--- Attempt_2.c
+++ Attempt_2.c
@@ -1,3 +1,34 @@
 // GASEOUS-GIGANTICUS PLUGIN-PORT FOR GIMP
 
-// - - - - - 
+// - - - - - ◊ - - - - - ◊ DECLARE NECESSARY LIBRARIES ◊ - - - - - ◊ - - - - - //
+// - - - - - ◊ C-language Universal Libraries ◊ - - - - - //
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include <errno.h>
+#include <limits.h>
+#include <pthread.h>
+#include <getopt.h>
+#include <locale.h>
+#include <sys/time.h>
+
+// - - - - - ◊ GIMP Plugin Required Libraries ◊ - - - - - //
+#include <libgimp/gimp.h>
+#include <libgimp/gimpui.h>
+
+#include "libgimp/stdplugins-intl.h"
+#include "config.h"
+
+// - - - - - ◊ Local Required Libraries ◊ - - - - - //
+#include "mtwist.h"
+#include "mathutils.h"
+#include "quat.h"
+#include "open-simplex-noise.h"
+#include "png_utils.h"
+#include "pthread_util.h"
